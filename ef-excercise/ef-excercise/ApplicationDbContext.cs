c78b2759e1e233d0f526b9e0e6eba5c6ace2b6ed
josue052015 <76499684+josue052015@users.noreply.github.com>
2023-01-11 22:59:41 -0400

    connection string

---
+++ ef-excercise/ef-excercise/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ef_excercise
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //options is for pass configurations through the class instance(like connection string)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ ef-excercise/ef-excercise/Program.cs
@@ -0,0 +1,33 @@
+using ef_excercise;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//the best practice to pass the connectionString is passing through a configuration provider,
+//that contains a json allowing our connection string instead of using hardcode here
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer("name=DefaultConnection"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
